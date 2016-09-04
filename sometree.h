c0565c848676b7af1c651fd5106e72e3c2c971f2
praktolock <praktolock@mail.ru>
2016-09-05 01:51:19 +1000

    Create sometree.h

---
+++ sometree.h
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+//  cat
+//  car
+//  card
+//  cow
+//  vc
+//
+//   (root)
+//      0-->c------->v
+//          |        |
+//          a----->o c
+//          |      | |
+//          r--->t w 0
+//          |    | |
+//          0->d 0 0
+//             |
+//             0
+
+struct NODE
+{
+  struct NODE*right,*bottom;
+  char value;
+};
+
+struct NODE*createnode(char _value)
+{
+  struct NODE*ret=malloc(sizeof(struct NODE));
+  ret->right=NULL;
+  ret->bottom=NULL;
+  ret->value=_value;
+}
+
+void deletetree(struct NODE*_node)
+{
+  if(_node->bottom)deletetree(_node->bottom);
+  if(_node->right) deletetree(_node->right);
+  free(_node);
+}
+
+struct NODE*placeright(struct NODE*_what, struct NODE*_where)
+{
+  _what->right=_where->right;
+  _where->right=_what;
+  return(_what);
+}
+
+struct NODE*hfind(struct NODE*_top, char _value) //horizontal find (-:
+{
+  struct NODE*cursor=_top;
+  while(cursor->value!=_value)
+  {
+    cursor=cursor->right;
+    if(!cursor)return(NULL);
+  }
+  return(cursor);
+}
+
+int checkstring(struct NODE*_top, char*_str)
+{
+  struct NODE*ncur=_top; //node cursor
+  char  *scur     =_str; //string cursor
+  while(1)
+  {
+    ncur=hfind(ncur, *scur);
+    if(!ncur)
+      return(0);
+    if(!*scur && !ncur->value)
+      return(1);
+    ncur=ncur->bottom;
+    scur++;
+  }
+  return(0);
+}
+
+int loadtree(char*filename, struct NODE**_top) //fill tree with strings from file
+{
+  struct NODE **cursor=_top;
+  FILE*fdesc=fopen(filename, "r");
+  if(fdesc==NULL)
+  {
+    printf("fopen error: %s\n", strerror(errno));
+    return(0);
+  }
+  while(!feof(fdesc))
+  {
+    char c;
+    c=fgetc(fdesc);
+    if(c < 32)c=0;
+    if(!*cursor)
+    {
+      *cursor=createnode(c);
+      if(c)
+        cursor=&(*cursor)->bottom;
+      else
+        cursor=_top;
+      continue;
+    }
+    while((*cursor)->value != c)
+    {
+      //find or create node with value == c, and set cursor to it
+      if(!*cursor)
+      {
+        *cursor=createnode(c);
+        continue;//break; //?
+      }
+      if((*cursor)->value > c)
+      {
+        struct NODE*tmp=createnode(c);
+        tmp->right=*cursor;
+        *cursor=tmp;
+        continue;
+      }
+      if((*cursor)->value < c)
+      {
+        cursor=&(*cursor)->right;
+        if(!*cursor)
+          *cursor=createnode(c);
+        continue;
+      }
+    }
+    if(c)
+      cursor=&(*cursor)->bottom;
+    else
+      cursor=_top;
+  }
+  fclose(fdesc);
+  return(1);
+}
+
+void printtree(struct NODE*_node, int lvl)
+{
+  static char tmpstr[2]={0, 0};
+  if(!_node)return;
+  int i=0;
+  while(lvl!=i++)
+    printf(" ");
+  tmpstr[0]=_node->value;
+  if(!_node->value)tmpstr[0]='0';
+  printf("%s\n", tmpstr);
+  printtree(_node->bottom, lvl+1);
+  printtree(_node->right,  lvl);
+}
