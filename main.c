f4c99b8a87b06311a34c8d503b1a8352e8165b75
praktolock <praktolock@mail.ru>
2016-11-24 01:25:15 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,32 @@
 #include "sometree.h"
 #include <stdio.h>
 
+#define INSTRINGLENGTH 256
+
 int main(int argc, char**argv)
 {
-  char instring[256];
-  if(argc<2)
-    printf("wrong params\n");
-  struct NODE*tree=NULL;
-  if(!loadtree(argv[1], &tree))
-    return(0);
+    char instring[INSTRINGLENGTH];
+    if(argc<2)
+        printf("wrong params\n");
+    struct NODE*tree=NULL;
+    if(!loadtree(argv[1], &tree))
+        return(0);
 
-  //printtree(tree, 0);
+    //printtree(tree, 0);
 
-  while(1)
-  {
-    printf(">");
-    if(scanf("%s", instring)==EOF)
-      break;
-    if(!strcmp(instring, "exit"))
-      break;
-    if(checkstring(tree, instring))
-      printf("YES\n");
-    else
-      printf("NO\n");
-  }
+    while(1)
+    {
+      if(!fgets(instring, INSTRINGLENGTH, stdin))
+          break;
+      sscanf(instring, "%[^\n]", instring);
+      if(!strcmp(instring, "exit"))
+          break;
+      if(checkstring(tree, instring))
+          printf("YES\n");
+      else
+          printf("NO\n");
+    }
 
-  deletetree(tree);
-  return(1);
+    deletetree(tree);
+    return(1);
 }
